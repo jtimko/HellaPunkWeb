6975f284589d57948079cc5df809116e642a7b41
jtimko <justin@jtimko.com>
2023-05-18 22:47:36 -0700

    Using email to get Id

--- app/post/add/route.ts
+++ app/post/add/route.ts
@@ -1,29 +1,38 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export async function POST (req: Request) {
-  const prisma = new PrismaClient()
+export async function POST(req: Request) {
+  const prisma = new PrismaClient();
 
   const body = await req.json();
 
+  const userId = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (!userId) return NextResponse.error();
+
   const data = await prisma.comments.create({
     data: {
-        userId: body.userId,
-        postId: body.postId,
-        message: body.comment,
-    }
+      userId: userId!.id,
+      postId: body.postId,
+      message: body.message,
+    },
   });
 
   const updatePost = await prisma.post.update({
     where: {
-        id: body.postId
-    }, 
+      id: body.postId,
+    },
     data: {
-        updatedAt: new Date()
-    }
+      updatedAt: new Date(),
+    },
   });
 
-  if (data && updatePost)
-    return NextResponse.json(data);
-};
-
+  if (data && updatePost) return NextResponse.json(data);
+}
